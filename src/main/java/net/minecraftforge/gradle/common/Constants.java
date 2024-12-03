9aa1fcd4437cdf22f369d21b24752eccab66ddc4
superJK92iscool <50285310+superJK92iscool@users.noreply.github.com>
2024-12-03 17:29:16 -0600

    Fix outdated URLs

--- src/main/java/net/minecraftforge/gradle/common/Constants.java
+++ src/main/java/net/minecraftforge/gradle/common/Constants.java
@@ -60,12 +60,12 @@ public class Constants
     public static final Closure<Boolean> CALL_FALSE = new Closure<Boolean>(null){ public Boolean call(Object o){ return false; }};
 
     // urls
-    public static final String MC_JAR_URL       = "http://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/{MC_VERSION}.jar";
-    public static final String MC_SERVER_URL    = "http://s3.amazonaws.com/Minecraft.Download/versions/{MC_VERSION}/minecraft_server.{MC_VERSION}.jar";
-    public static final String MCP_URL          = "http://files.minecraftforge.net/fernflower_temporary.zip";
-    public static final String ASSETS_URL       = "http://resources.download.minecraft.net";
+    public static final String MC_JAR_URL       = "https://launcher.mojang.com/v1/objects/{hash}/client.jar";
+    public static final String MC_SERVER_URL    = "https://launcher.mojang.com/v1/objects/{hash}/server.jar";
+    public static final String MCP_URL          = "https://files.minecraftforge.net/fernflower_temporary.zip";
+    public static final String ASSETS_URL       = "https://resources.download.minecraft.net/legacy";
     public static final String LIBRARY_URL      = "https://libraries.minecraft.net/";
-    public static final String ASSETS_INDEX_URL = "https://s3.amazonaws.com/Minecraft.Download/indexes/{ASSET_INDEX}.json";
+    public static final String ASSETS_INDEX_URL = "https://piston-meta.mojang.com/mc/assets/legacy/c0fd82e8ce9fbc93119e40d96d5a4e62cfa3f729/{ASSET_INDEX}.json";
 
     public static final String LOG              = ".gradle/gradle.log";
     public static final String ASSETS_INDEX     =  "legacy";
